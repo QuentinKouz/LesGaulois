d4f6947e0de56857d0cc1120f7649182caeda5a3
kouzmitch quentin <kzq4327a@ens.fsi>
2022-10-17 16:20:46 +0200

    tp2Done

--- src/personnages/Equipement.java
+++ src/personnages/Equipement.java
@@ -1,5 +1,20 @@
 package personnages;
 
 public enum Equipement {
-	CASQUE,BOUCLIER
+	CASQUE("casque"),BOUCLIER("bouclier");
+	public String nom;
+	
+	Equipement(String nomEquipment) {
+		this.nom = nomEquipment;
+	}
+	
+	@Override
+	public String toString() {
+		return "Equipement [nom=" + nom + "]";
+	}
+	
+	public static void main(String[] args) {
+		Equipement casque = Equipement.CASQUE;
+		System.out.println(casque.nom);
+	}
 }
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -4,9 +4,11 @@ import personnages.Equipement;
 public class Romain {
 	private String nom;
 	private int force;
+	Equipement[] equipements = new Equipement[2];
+	private int nbEquipement = 0;
 	
 	public Romain(String nom, int force) {
-		assert (force >= 0): "La force est négative";
+		assert (force >= 0): "La force est nï¿½gative";
 		this.nom = nom;
 		this.force = force;
 	}
@@ -16,7 +18,7 @@ public class Romain {
 	}
 	
 	public void parler(String texte) {
-		System.out.println(prendreParole() + "« " + texte + "»");
+		System.out.println(prendreParole() + "ï¿½ " + texte + "ï¿½");
 	}
 	
 	private String prendreParole() {
@@ -26,17 +28,42 @@ public class Romain {
 	public void recevoirCoup(int forceCoup) {
 		force -= forceCoup;
 		if (force > 0) {
-			parler("Aïe");
+			parler("Aï¿½e");
 		} else {
 			parler("J'abandonne...");
+		}
 	}
+	
+
+	public void sEquiper(Equipement equipement) {
+		switch (nbEquipement) {
+			case 2: 
+				System.out.println("Le soldat " + this.getNom() + " est déjà bien protégé !");
+				break;
+			case 1:
+				if (equipements[0] == equipement) {
+					System.out.println("Le soldat " + this.getNom() + " possède déjà un " + equipement.nom);
+					break;
+				}
+			default:
+				equipements[nbEquipement] = equipement;
+				nbEquipement += 1;
+				System.out.println("Le soldat " + this.getNom() + " s'équipe avec un " + equipement.nom + ".");
+				break;
+				
+		}
+				
+			
 	}
 	
 		public static void main(String[] args) {
-			System.out.println("test");
-			Romain romain = new Romain("Romain", 6);
-			System.out.println(Equipement.BOUCLIER);
-			System.out.println(Equipement.CASQUE);
+			Romain minus = new Romain("Minus",5);
+			minus.sEquiper(Equipement.CASQUE);
+			minus.sEquiper(Equipement.CASQUE);
+			minus.sEquiper(Equipement.BOUCLIER);
+			minus.sEquiper(Equipement.BOUCLIER);
+			
+			
 		}
 
 }
